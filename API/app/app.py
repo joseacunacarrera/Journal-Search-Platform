67ac30886832010614f90d1f0b2df8c9b0a29745
Valeria Sell <70655662+valeria-sell@users.noreply.github.com>
2022-11-27 13:21:53 -0600

    testing methods // API
    testing obtener articulos, likear articulos y articulos gustados

--- API/app/app.py
+++ API/app/app.py
@@ -1,10 +1,20 @@
 from flask import *
 from wrapper_mariadb import MariaDB
 from elasticsearch import Elasticsearch
+import firebase_admin
+from firebase_admin import credentials, db, auth
 import json, time
 
     
 app = Flask(__name__)
+config = {
+    'databaseURL': "https://journal-search-platform-default-rtdb.firebaseio.com",
+    'projectId': "journal-search-platform",
+    'storageBucket': "journal-search-platform.appspot.com",
+}
+
+cred = credentials.Certificate("credentials.json")
+default_app = firebase_admin.initialize_app(cred, options = config)
 
 mariadb_instance = MariaDB('127.0.0.1', 'mypass', 'root', 'mydb')
 
@@ -82,19 +92,93 @@ def buscar_articulos():
         print(hit["_source"]["rel_title"], hit["_id"])
         lis_res.append({'Title': hit['_source']['rel_title'], 'Id': hit['_id']})
 
-    return lis_res
+    response = json.dumps(lis_res)
+    return make_response(response)
 
 @app.route('/articulos/obtener', methods=['GET'])
 def obtener_articulo():
-    return"Obtener Articulo"
+    search_input = str(request.args.get('search_input'))
+    article_title = search_input
+    ELASTIC_PASSWORD = "mypass"
+    es = Elasticsearch("https://localhost:9200", basic_auth=('elastic',ELASTIC_PASSWORD), verify_certs = False)
+
+    query_body = {
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "span_near": {
+                            "clauses": [
+                                {
+                                    "span_multi": {
+                                        "match": {
+                                            "fuzzy": {
+                                                "rel_title":{
+                                                    "value": article_title,
+                                                    "fuzziness": "AUTO"
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            ],
+                            "slop": 0,
+                            "in_order": False
+                        }
+                    }
+                ]
+            }
+        }
+    }
+    resp = es.search(index="groups", body=query_body, size=100)
+    
+    return resp
 
 @app.route('/articulos/like', methods=['POST'])
 def likear_articulo():
-    return"Likear Articulo"
+    article = str(request.args.get('user_input'))
+    user_id = str(request.args.get('user_id'))
+
+    data = {'user_id': user_id, 'article': article}
+
+    body = json.loads(data)
+    try:
+        ref = db.reference("/likes")
+        ref.push(body)
+        return make_response('Like registrado')
+    except Exception as error:
+        return f'Error: {error}'
     
 @app.route('/artuculos/gustados', methods=['GET'])
 def articulos_gustados():
-    return"Articulos gustados"
+    user_id = request.args.get('user_id')
+    try:
+        ref = db.reference(f'/likes')
+
+        likes = res_to_list(ref.get())
+        response = json.dumps(filtrar_likes_usuario(likes=likes, user_id=user_id))
+        return make_response(response)
+    except Exception as error:
+        return f'Error: {error}'
+
+
+
+def filtrar_likes_usuario(likes, user_id):
+
+    lista_likes = []
+    for like in likes:
+        if like['user_id'] == user_id:
+            lista_likes.append(like)
+
+    return lista_likes
+
+def res_to_list(res):
+    fb_keys = list(res.keys())
+
+    for fb_key, value in zip(fb_keys, res.values()):
+        value['fb_key'] = fb_key
+
+    return list(res.values())
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000)
